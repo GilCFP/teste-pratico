5a2bcbb8173aea51df50c28a27a07a05e47f4eba
GilCFP <87478201+GilCFP@users.noreply.github.com>
2024-03-07 20:05:29 -0300

    exibindo a respose para o usuário

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,43 @@ import './App.css'
 import React from 'react'
 import { Container, Row} from 'react-bootstrap'
 import DataForm from './form'
+import { useState, useEffect } from 'react'
+
 function App() {
 
+  const [response, setResponse] = useState(null);
+  const [fade, setFade] = useState(false);
+
+  const insertData = (data) => {
+    setResponse(data);
+  };
+
+  useEffect(() => {
+    if (response) {
+      setFade(true);
+      const timeout = setTimeout(() => {
+        setFade(false);
+      }, 500); // Tempo da animação
+
+      return () => clearTimeout(timeout);
+    }
+  }, [response]);
+
   return (
-      <div className='App'>
-        <Container class>
-          <Row>
-            <DataForm/>
-          </Row>
-          <Row id="Response-insert">
-          </Row>
-        </Container>
-      </div>
-  )
+    <div className='App'>
+      <Container>
+        <Row>
+          <DataForm insertData={insertData} />
+        </Row>
+        <Row id="response-insert" className={`mt-4 ${fade ? 'fade-in-out' : ''}`}>
+          {response && (
+            <>O Petshop mais barato é <br /><b id='petshopname'>{response.name}</b><br />e o preço total é <br /><b id='price'>R${response.price}</b></>
+          )}
+        </Row>
+      </Container>
+    </div>
+  );
 }
+
 
 export default App
